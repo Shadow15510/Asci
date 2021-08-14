79b76b0ea314243ca4d1cdc816cb37a00196af13
Shadow15510 <antoine.royer31@orange.fr>
2021-08-14 23:27:05 +0200

    Clean the repo

--- new_project/opus_1.py
+++
@@ -1,7 +0,0 @@
-from asci_lib import *
-
-print("Opus 1 (v1.1b):\n'Le Secret d'Asci'\nNouvelle partie:\nasci()\nContinuer :\nasci('...')")
-
-def asci(code="0.100.1.1"):
-	game = Asci(code, 1)
-	game.mainloop()
